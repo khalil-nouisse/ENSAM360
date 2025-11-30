e76e994435c643617f347ba8939594ce5c00a174
OmarBouhlal <omarbouhlal05@gmail.com>
2025-11-30 14:01:49 +0100

    fix : toggle button of color mode initial color

--- frontend/src/components/mode-toggle.jsx
+++ frontend/src/components/mode-toggle.jsx
@@ -10,7 +10,7 @@ export function ModeToggle() {
             variant="ghost"
             size="icon"
             onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-            className="rounded-full"
+            className="rounded-full text-foreground"
             title="Toggle theme"
         >
             <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
